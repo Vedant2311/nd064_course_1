dc9eb456c13c35b538ce7104ad87c52b70adcb9d
Vedant Sanjay Raval <raval@Vedants-MacBook-Pro.local>
2022-01-26 20:46:41 +0530

    Wrapping up until step-2 of Techtrends project

--- project/techtrends/app.py
+++ project/techtrends/app.py
@@ -2,12 +2,20 @@ import sqlite3
 
 from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
 from werkzeug.exceptions import abort
+import logging
+import sys
+
+# Some global variables used for the metrics functionality
+db_connection_count = 0
+post_count = 0
 
 # Function to get a database connection.
 # This function connects to database with the name `database.db`
 def get_db_connection():
+    global db_connection_count
     connection = sqlite3.connect('database.db')
     connection.row_factory = sqlite3.Row
+    db_connection_count+=1
     return connection
 
 # Function to get a post using its ID
@@ -22,30 +30,35 @@ def get_post(post_id):
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your secret key'
 
-# Define the main route of the web application 
+# Define the main route of the web application
 @app.route('/')
 def index():
+    global post_count
     connection = get_db_connection()
     posts = connection.execute('SELECT * FROM posts').fetchall()
     connection.close()
+    post_count = len(posts)
     return render_template('index.html', posts=posts)
 
-# Define how each individual article is rendered 
+# Define how each individual article is rendered
 # If the post ID is not found a 404 page is shown
 @app.route('/<int:post_id>')
 def post(post_id):
     post = get_post(post_id)
     if post is None:
-      return render_template('404.html'), 404
+        logging.error("A non-existent article was attempted to be retrived")
+        return render_template('404.html'), 404
     else:
-      return render_template('post.html', post=post)
+        logging.info("Post viewed has the title " + post['title'])
+        return render_template('post.html', post=post)
 
 # Define the About Us page
 @app.route('/about')
 def about():
+    logging.info("About us page was retrived successfully")
     return render_template('about.html')
 
-# Define the post creation functionality 
+# Define the post creation functionality
 @app.route('/create', methods=('GET', 'POST'))
 def create():
     if request.method == 'POST':
@@ -60,11 +73,39 @@ def create():
                          (title, content))
             connection.commit()
             connection.close()
-
+            
+            logging.info("Post created successfully with the title: " + title)
             return redirect(url_for('index'))
 
     return render_template('create.html')
 
+# Define the health endpoint
+@app.route('/healthz')
+def status():
+    response = app.response_class(
+        response=json.dumps({"result":"OK - healthy"}),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
+
+# Define the metrics endpoint
+@app.route('/metrics')
+def metrics():
+    response = app.response_class(
+        response=json.dumps({"post_count":post_count, "db_connection_count": db_connection_count}),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
+
 # start the application on port 3111
 if __name__ == "__main__":
-   app.run(host='0.0.0.0', port='3111')
+    logging.basicConfig(
+        level=logging.INFO,
+        handlers=[
+            logging.FileHandler("app.log"),
+            logging.StreamHandler()
+        ]
+    )
+    app.run(debug=True,host='0.0.0.0', port='3111')
